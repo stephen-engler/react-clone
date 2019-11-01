333523cb51d9a7b8a29c5d104e37be9b8c75293b
Stephen Engler <sengler1989@gmail.com>
2019-11-01 15:21:27 -0500

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.tsx',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        }, ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
     },
     watch: true,
     devtool: 'source-map',
